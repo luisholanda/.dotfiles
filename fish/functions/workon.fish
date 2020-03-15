9c9ec3bd36f1490ef9b86b37e1c8f6201e4e634d
Luis Holanda <luiscmholanda@gmail.com>
2020-03-15 16:24:59 -0300

    fish: active pyenv virtualenv on workon command

--- fish/functions/workon.fish
+++ fish/functions/workon.fish
@@ -1,9 +1,12 @@
 function workon --description 'Goto the given project' --argument project
-    if test -d ~/TerraMagna/repositories/$project
-cd ~/TerraMagna/repositories/$project
-else if test -d ~/Projects/$project
-cd ~/Projects/$project
-else
-echo "Project $project not found"
-end
+  if test -d ~/TerraMagna/repositories/$project
+    cd ~/TerraMagna/repositories/$project
+  else if test -d ~/Projects/$project
+    cd ~/Projects/$project
+  else
+    echo "Project $project not found"
+    exit 1
+  end
+
+  pyenv activate $project
 end
