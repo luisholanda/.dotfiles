325568a8c42f9359c8bf9ba982f1896f41583276
Luis Holanda <luiscmholanda@gmail.com>
2020-12-17 19:32:42 -0300

    yabai: remove unused yabai_scripts folder

--- yabai_scripts/jetbrains_float.sh
+++
@@ -1,5 +0,0 @@
-if yabai -m --windows \
-  | jq -er 'map(select(.id == env.YABAI_WINDOW_ID).pid)[0] as $pid | map(select(.pid == $pid)) | length == 1' > /dev/null
-then
-  yabai -m window "${YABAI_WINDOW_ID}" --toggle float
-fi
