1e6cb08b0c8251e06a0a8a6fa2d3fae3c09ccc03
Luis Holanda <luiscmholanda@gmail.com>
2021-01-18 09:54:32 -0300

    nix: update Firefox to 84.0.2

--- nix/overlays/firefox.nix
+++ nix/overlays/firefox.nix
@@ -2,7 +2,7 @@ self: super:
 {
   firefox = self.installApplication rec {
     name = "Firefox";
-    version = "84.0.1";
+    version = "84.0.2";
 
     description = "The Firefox web browser";
     homepage = "https://www.mozilla.org/en-US/firefox/";
@@ -10,7 +10,7 @@ self: super:
     src = super.fetchurl {
       name = "${name}-${version}.dmg";
       url = "https://ftp.mozilla.org/pub/firefox/releases/${version}/mac/en-US/Firefox%20${version}.dmg";
-      sha256 = "159jkkyzhsq66c0fvq10yvaq03wahg8xxhhvrqj58r74qg4c93wy";
+      sha256 = "1xycm3v8ix1lg91qv3kwihqbvq877crx88jn9vqaczvzl1zrdbic";
     };
   };
 }
