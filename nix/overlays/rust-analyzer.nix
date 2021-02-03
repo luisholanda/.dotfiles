eec7de3283c3dabfef5cf52d6dada2fae881dfa1
Luis Holanda <luiscmholanda@gmail.com>
2021-02-03 09:01:34 -0300

    nix: update rust-analyzer

--- nix/overlays/rust-analyzer.nix
+++ nix/overlays/rust-analyzer.nix
@@ -2,13 +2,13 @@ self: super:
 {
   rust-analyzer = with super; stdenv.mkDerivation rec {
     name = "rust-analyzer";
-    version = "2021-01-11";
+    version = "2021-01-25";
 
     src = with stdenv; fetchurl rec {
       name = "rust-analyzer-${if isDarwin then "mac" else "linux"}.gz";
       url = "https://github.com/rust-analyzer/rust-analyzer/releases/download/${version}/${name}";
       sha256 = if isDarwin
-        then "0f9nwhn7w5kka2vwrbavxb7r1j1ajp18l3a944bxxh0889rsz0cp"
+        then "1i5f7jl7qjzykql22i3yrhv0nkj0bppxk5yxbgi0j0fzgxc4nwk1"
         else "0000000000000000000000000000000000000000000000000000";
     };
 
