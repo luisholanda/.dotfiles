032c3e7940ec3b1a6db98c07582cd527ea39434b
Luis Holanda <luiscmholanda@gmail.com>
2021-06-01 12:40:14 -0300

    nix: remove vendored osu-lazer

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -3,5 +3,4 @@ self: super:
   bmono = import ./data/fonts/bmono self;
   # TODO: try remove self.xorg
   brave = import ./networking/browsers/brave (self // self.xorg);
-  osu-lazer = import ./games/osu-lazer self;
 }
